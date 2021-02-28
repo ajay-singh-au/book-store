bb422688f3b21633fbb6435dfeed88fcb0095e39
ajay-singh-au <ajay.singh@accolitedigital.com>
2021-02-28 16:53:36 +0530

    feature 1: booklist with individual route:complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Book from './component/Book'
 import BookDetails from './component/BookDetails'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 const App = () => {
   return (
     <>
       <Router>
-        <h1 className="text-white bg-dark p-2 text-center">Book Store</h1>
+        <Link to="/">
+          <h1 className="text-white bg-dark p-2 text-center">Book Store</h1>
+        </Link>
         <Route exact path="/" component={Book} />
         <Route path="/:book_id" component={BookDetails} />
       </Router>
